30ed2a9719a16c133bf63fd1a81c5947573d2aa1
takemasa <takemasa@val.co.jp>
2013-11-07 10:46:57 +0900

    REST APIにアクセスするメソッドのテストをmockで記述

--- lib/collection.rb
+++ lib/collection.rb
@@ -1,9 +1,21 @@
 # coding: utf-8
 require 'bundler/setup'
-Bundler.setup
 Bundler.require
-require 'yaml'
-require 'fileutils'
 
 class Collector
+
+    def initialize(keyword = nil, account_num = nil, since_id = nil)
+        @keyword = keyword
+        @account_num = account_num
+    end
+
+    def search_tw
+        return "success"
+    end
+
+    def file(keyword)
+        file = @keyword.search_tw
+        return "status #{file}"
+    end
+
 end
--- spec/collection_spec.rb
+++ spec/collection_spec.rb
@@ -4,10 +4,14 @@ describe Collector do
   after(:each) do
     system 'rm -rf ./tweet/*'
   end
-  describe "searchメソッド" do
-  	it 'は、キーワードを指定した場合、一致するツイートを取得する' do
-  		Collector.new("なう").search
-  		expect(FileTest.exist?("./tweet/now/*_now.csv")).to eq(true)
-  	end
+
+  describe 'file' do
+
+    it 'はREST APIにアクセスする' do
+      rest = double('keyword')
+      rest.should_receive(:search_tw).and_return('success')
+      collector = Collector.new(rest)
+      expect(collector.file(@keyword)).to eq('status success')
+    end
   end
 end
