3a8899ad2bc95c8c3869dfe2325f78e9ea441c17
takemasa <takemasa@val.co.jp>
2013-11-08 12:08:24 +0900

    OAuth認証を行う処理とそのテストを作成

---
+++ lib/collection.rb
@@ -0,0 +1,35 @@
+# coding: utf-8
+require 'bundler/setup'
+Bundler.require
+
+class Collector
+
+    def initialize(keyword)
+        @keyword = keyword
+        @since_id = 0
+        @text = nil
+    end
+
+    def search_tweet(client)
+        client.search(@keyword, :count => 100, :result_type => "recent", :since_id => @since_id, :lang=>"ja").results.reverse.each do |tweet|
+            return tweet
+        end
+    end
+end
+
+class Authenticate
+
+    def initialize(account_num)
+        @account_num = account_num
+    end
+
+    def twconfig(twitter = Twitter::Client)
+        twaccount = YAML.load_file('./config/twaccount.yaml')
+        client = twitter.new(
+            :consumer_key => twaccount["consumer_key#{@account_num}"],
+            :consumer_secret => twaccount["consumer_secret#{@account_num}"],
+            :oauth_token => twaccount["oauth_token#{@account_num}"],
+            :oauth_token_secret => twaccount["oauth_token_secret#{@account_num}"]
+            )
+    end
+end
