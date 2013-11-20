c971f341e014e1fffb530e049453b8363cf021df
takemasa <takemasa@val.co.jp>
2013-11-20 11:59:36 +0900

    seach実行時のパラメータを変数化

--- lib/collector.rb
+++ lib/collector.rb
@@ -3,14 +3,17 @@
 
 class Collector
 
-    def initialize(keyword)
+    def initialize(keyword , lang = 'ja', result_type = "recent", count = 100)
         @keyword = keyword
         @since_id = 0
+        @lang = lang
+        @result_type = result_type
+        @count = count
     end
 
-    def search_tweet(client)
+    def search_tweet(client) # searchによって取得したTwitter::Tweetクラスのオブジェクトを100件返す
         @tweet = []
-        client.search(@keyword, :count => 100, :result_type => "recent", :since_id => @since_id, :lang=>"ja").results.reverse.each do |tweet|
+        client.search(@keyword, :count => 100, :result_type => @result_type, :since_id => @since_id, :lang=>@lang).results.reverse.each do |tweet|
             @tweet << tweet
         end
         rescue Twitter::Error => e
