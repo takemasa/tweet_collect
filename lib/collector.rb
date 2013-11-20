aedafebf8c2990cabafd6f472cd3a5b8e6df0530
takemasa <takemasa@val.co.jp>
2013-11-20 09:52:44 +0900

    ツイート取得時に発生したエラーの詳細を表示させるよう修正

--- lib/collector.rb
+++ lib/collector.rb
@@ -13,6 +13,8 @@ class Collector
         client.search(@keyword, :count => 100, :result_type => "recent", :since_id => @since_id, :lang=>"ja").results.reverse.each do |tweet|
             @tweet << tweet
         end
+        rescue Twitter::Error => e
+            raise "error_time:#{Time.now}", "class:#{e.class}  message:#{e.message}"
     end
      attr_accessor :tweet
 end
