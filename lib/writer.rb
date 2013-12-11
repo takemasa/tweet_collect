30184fcdeefcad6506b2ae1bdd724b4c10976504
takemasa <takemasa@val.co.jp>
2013-12-11 19:36:42 +0900

    曜日の標記を再修正

--- lib/writer.rb
+++ lib/writer.rb
@@ -6,7 +6,7 @@ class Writer
         @requested_output_name = requested_output_name
         @output_name = get_output_name
         @day = Time.now
-        @wdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+        @wdays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
     end
     attr_reader :output_name
 
