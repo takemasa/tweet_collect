cc9d8ea88f77b569c3ad61f91d2a221e3be3ea90
takemasa <takemasa@val.co.jp>
2013-12-12 21:43:53 +0900

    2つの変数をインスタンス変数に変更しinitializeメソッドを作成

--- lib/compressor.rb
+++ lib/compressor.rb
@@ -3,21 +3,29 @@ Bundler.require
 
 class Compressor
 
+    def initialize
+        @old_files = get_old_filename
+        @gzip_files = []
+    end
+    attr_reader :old_files
+    attr_reader :gzip_files
+
     def get_old_filename
         day = Time.now
         date = "#{day.year}-#{day.month}-#{day.day}"
-        old = []
-
+        olds = []
         Dir.glob("./tweet/data/**/*.ltsv").each {|all_ltsv|
-                old << all_ltsv unless all_ltsv.include?(date)
+                olds << all_ltsv unless all_ltsv.include?(date)
         }
-        old
+        olds
     end
 
-    def compress_file(filenames)
-        filenames.each{|filename|
+    def compress_file
+        self.old_files.each{|filename|
             file = File.open(filename).read
-            Zlib::GzipWriter.open("#{filename}.gz") {|gz|
+            compress_file = "#{filename}.gz"
+            self.gzip_files << compress_file
+            Zlib::GzipWriter.open(compress_file) {|gz|
                 gz.write file
             }
         }
--- spec/compressor_spec.rb
+++ spec/compressor_spec.rb
@@ -2,23 +2,25 @@ require 'compressor'
 
 describe Compressor do
 
-    after(:each) do
-        system "rm -f ./tweet/data/test/*"
-    end
+    old_filename = "./tweet/data/test/#{Time.now.year}/#{Time.now.month}/#{Time.now.year}-#{Time.now.month}-#{Time.now.day-1}_test.ltsv"
+    new_filename = "./tweet/data/test/#{Time.now.year}/#{Time.now.month}/#{Time.now.year}-#{Time.now.month}-#{Time.now.day}_test.ltsv"
+    File.open("#{old_filename}",'a+')
+    File.open("#{new_filename}",'a+')
 
-    filename = "./tweet/data/test/#{Time.now.year}/#{Time.now.month}/#{Time.now.year}-#{Time.now.month}-#{Time.now.day-1}_test.ltsv"
 
     describe 'get_old_filename' do
-        it 'は現在時刻がファイル名に含まれないファイrの一覧を取得' do
-            File.open("#{filename}",'a+')
-            expect(Compressor.new.get_old_filename).to eq([filename])
+        it 'は現在の日付がファイル名に含まれないファイrの一覧を取得' do
+            expect(Compressor.new.old_files).to eq([old_filename])
         end
     end
 
     describe 'compress_file' do
         it '受け取ったファイル名に基づいてファイルをgzip圧縮する' do
-            expect(Compressor.new.compress_file([filename])).to eq(true)
-            expect(FileTest.exist?("#{filename}.gz")).to eq(true)
+            Compressor.new.compress_file
+            expect(FileTest.exist?("#{old_filename}.gz")).to eq(true)
+        end
+    end
+
         end
     end
 
