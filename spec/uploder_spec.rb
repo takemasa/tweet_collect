4ea598c9a77c5515110f923323081b14c57fab98
takemasa <takemasa@val.co.jp>
2013-12-18 17:30:21 +0900

    ファイルをs3にアップロードするメソッドを作成

--- spec/uploder_spec.rb
+++ spec/uploder_spec.rb
@@ -13,4 +13,23 @@ describe Uploder do
     File.open("#{old_filename}",'a+')
     File.open("#{new_filename}",'a+')
 
+    describe 'get_dir' do
+        it 'はファイルのアップロード先を返す' do
+            expect(Uploder.new(old_filename).get_dir(old_filename)).to eq("dsb-twitter/test/#{Time.now.year}/#{Time.now.month}/#{File.basename(old_filename)}")
+        end
+    end
+
+    describe 'upload' do
+        it 'はファイルをs3にアップロードする' do
+            m_buckets = double('buckets')
+            m_objects = double('objects')
+            m_upload = double('upload')
+
+            m_buckets.should_receive(:buckets).and_return(m_objects)
+            m_objects.should_receive(:objects).and_return(m_upload)
+            m_upload.should_receive(:write).with({:file => "dirname"}).and_return(['results'])
+
+            expect(Uploder.new(old_filename).upload(m_buckets, "dirname")).to eq(['results'])
+        end
+    end
 end
