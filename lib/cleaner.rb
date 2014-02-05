799752134c9b219079f838c9f9162b09e8b02c1d
takemasa <takemasa@val.co.jp>
2014-02-05 14:54:13 +0900

    ユーザプロフィールページへのリンクを追加

--- lib/cleaner.rb
+++ lib/cleaner.rb
@@ -48,30 +48,31 @@ class Cleaner
 
     def create_ary_tweet (tweet, client, retweeted, place_status, place)
 
-        created_at = set_label('created_at ', tweet.created_at) #ツイート日時
-        tweet_id = set_label('tweet_id ', tweet.id) #ツイートID
-        user_name = set_label('user_name ', tweet.user.screen_name) #ツイートしたユーザ名
-        user_id = set_label('user_id ', tweet.user.id) #ユーザID
-        user_profile = set_label('profile ', modify_tweet_status_str(tweet.user.description)) #ユーザのプロフィール
-        prof_url = set_label('prof_url ', modify_prof_urls(tweet.attrs[:user][:entities])) #プロフィール文のURL
-        text = set_label('text ', modify_tweet_status_str(tweet.full_text)) #ツイート本文
-        text_url = set_label('text_url ', modify_text_urls(tweet.attrs[:entities])) #ツイートのURL
-        client = set_label('client ', client) #ツイート時に使用したクライアント
-        place_status = set_label('place_status ', place_status) #位置情報の有無
-        place = set_label('place_city ', place) #位置情報
-        friends_count = set_label('friends_count ', tweet.user.friends_count) #フォロー数
-        followers_count = set_label('followers_count ', tweet.user.followers_count) #フォロワー数
-        all_tweet_count = set_label('all_tweet_count ', tweet.user.statuses_count) #総ツイート数
-        retweeted_status = set_label('retweeted ', retweeted) #リツイートであるか
-        retweet_count = set_label('retweet_count ', tweet.retweet_count) #リツイートされた回数
-        retweeted_user = set_label('retweeted_user ', tweet.retweeted_status.user.screen_name) if retweeted #リツイート元のユーザ名
+        created_at = set_label('created_at', tweet.created_at) #ツイート日時
+        tweet_id = set_label('tweet_id', tweet.id) #ツイートID
+        text = set_label('text', modify_tweet_status_str(tweet.full_text)) #ツイート本文
+        text_url = set_label('text_url', "#{modify_text_urls(tweet.attrs[:entities])}") #ツイートのURL
+        user_name = set_label('user_name', tweet.user.screen_name) #ツイートしたユーザ名
+        user_id = set_label('user_id', tweet.user.id) #ユーザID
+        user_page = set_label('user_page', "https://twitter.com/intent/user?user_id=#{tweet.user.id}") #ユーザーページのURL
+        user_profile = set_label('profile', modify_tweet_status_str(tweet.user.description)) #ユーザのプロフィール
+        prof_url = set_label('prof_url', "#{modify_prof_urls(tweet.attrs[:user][:entities])}") #プロフィール文のURL
+        client = set_label('client', client) #ツイート時に使用したクライアント
+        place_status = set_label('place_status', place_status) #位置情報の有無
+        place = set_label('place_city', place) #位置情報
+        friends_count = set_label('friends_count', tweet.user.friends_count) #フォロー数
+        followers_count = set_label('followers_count', tweet.user.followers_count) #フォロワー数
+        all_tweet_count = set_label('all_tweet_count', tweet.user.statuses_count) #総ツイート数
+        retweeted_status = set_label('retweeted', retweeted) #リツイートであるか
+        retweet_count = set_label('retweet_count', tweet.retweet_count) #リツイートされた回数
+        retweeted_user = set_label('retweeted_user', tweet.retweeted_status.user.screen_name) if retweeted #リツイート元のユーザ名
         case @ary_type
         when 'simple'
             ary = [created_at, user_name, text]
         when 'numeric'
             ary = [created_at, user_id, tweet_id, retweet_count, friends_count, followers_count, all_tweet_count]
         when 'all'
-            ary = [created_at, tweet_id, user_name, user_id, user_profile, prof_url, text, text_url, client, place_status, place, friends_count, followers_count, all_tweet_count, retweeted_status, retweet_count, retweeted_user]
+            ary = [created_at, tweet_id, text, text_url, user_name, user_id, user_page, user_profile, prof_url, client, place_status, place, friends_count, followers_count, all_tweet_count, retweeted_status, retweet_count, retweeted_user]
         end
         ary
     end
