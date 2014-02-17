3ea792363436601398c9cff7ce7e5f687e97a176
takemasa <takemasa@val.co.jp>
2014-02-17 17:56:56 +0900

    S3から指定ファイルをダウンロードするtweet_downloadを作成

---
+++ bin/tweet_download.rb
@@ -0,0 +1,25 @@
+# tweet_collect/config/search_config.yamlのfrom,to,keywordを指定してから実行
+require_relative '../lib/authenticater.rb'
+require_relative '../lib/getrefinesearchconfig.rb'
+require_relative '../lib/gettweetfroms3.rb'
+require 'bundler/setup'
+Bundler.require
+
+
+client = Authenticater.new.get_aws_client
+filepath = GetRefineSearchConfig.new.create_filepath
+files = []
+filepath.each do |file|
+    if GetTweetFromS3.new.avoid_duplication(file)
+        puts "#{file}*.ltsv exists!"
+    else
+        puts "#{file}*.ltsv not exists"
+    end
+    files << GetTweetFromS3.new.download(client, file) unless GetTweetFromS3.new.avoid_duplication(file)
+end
+
+puts ""
+files.each do |f|
+    puts "got #{f}" if f
+end
+puts GetTweetFromS3.new.file_exists?(files)
