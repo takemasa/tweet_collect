8adf32a71b3847be5a4b069025ccecf1ce2d0ff8
takemasa <takemasa@val.co.jp>
2014-03-04 18:00:34 +0900

    ツイートの取得件数に応じてAPIアクセス回数を変更する機能を削除

--- bin/tweet_collect.rb
+++ bin/tweet_collect.rb
@@ -15,7 +15,7 @@ error_message = nil
 ary_all_tweets = []
 ary_id = []
 ary_log = []
-length = 100
+length = nil
 
 # 同時刻の多重起動を防ぐため取得の実行前にsleep
 sleep_time = account_id.to_i % 9
@@ -23,37 +23,32 @@ sleep(sleep_time)
 execute_time = Time.now
 5.times do
     tweet_id = nil
-    if length != 0 # 前回実行時の取得数が10件以上か、前回取得を行わなかった場合にのみ次の取得を行う
-        result = collector.search_tweet(client, execute_time)
+    result = collector.search_tweet(client, execute_time)
     # 出力先をツイートとエラーのどちらにするかを判断
-        result.class == Array ? tweets = result : error_message = result
-        if tweets
-            length = tweets.length
-            ary_log << length
-            tweets.each do |tweet|
-                ary_tweet = []
-                tweet_id = tweet[:id]
+    result.class == Array ? tweets = result : error_message = result
+    if tweets
+        length = tweets.length
+        ary_log << length
+        tweets.each do |tweet|
+            ary_tweet = []
+            tweet_id = tweet[:id]
          # ツイートが取得できていればそのidをインスタンス変数since_idに代入
-                collector.since_id = tweet_id if tweet_id
+            collector.since_id = tweet_id if tweet_id
 
-                ary_tweet = cleaner.create_ary_tweet(tweet)
-                tweet = cleaner.join_tweet_status(ary_tweet)
-                ary_all_tweets << tweet
-            end
-    # ツイートが1件でも取得できていればそのidをid配列に格納
-            ary_id << tweet_id if tweet_id
-    # ファイルへの出力処理
-            writer.output_tweet(ary_all_tweets)
-            unless ary_id.empty?
-                collector.clear_idfile
-                writer.output_id(ary_id)
-            end
+            ary_tweet = cleaner.create_ary_tweet(tweet)
+            tweet = cleaner.join_tweet_status(ary_tweet)
+            ary_all_tweets << tweet
         end
-        writer.output_error(error_message) unless error_message.nil?
-    else
-        length = 100
-        ary_log << nil
+    # ツイートが1件でも取得できていればそのidをid配列に格納
+        ary_id << tweet_id if tweet_id
+    # ファイルへの出力処理
+        writer.output_tweet(ary_all_tweets)
+        unless ary_id.empty?
+            collector.clear_idfile
+            writer.output_id(ary_id)
+        end
     end
+    writer.output_error(error_message) unless error_message.nil?
     sleep(8)
     ary_all_tweets = []
     ary_id = []
