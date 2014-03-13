8e3de80b010cf0a4890542bd2c8711faee992d9e
takemasa <takemasa@val.co.jp>
2014-03-13 10:08:45 +0900

    ltsnをcsvに変換する機能の実行スクリプトを作成

--- bin/ltsv_parse_csv.rb
+++ bin/ltsv_parse_csv.rb
@@ -4,11 +4,13 @@ require 'bundler/setup'
 
 filenames = []
 puts 'Convert filename?(./refine_search/*)'
-puts 'Input "exit"'
+puts 'Input "end"'
 while true
     convert_filename = STDIN.gets.chomp
-    break if convert_filename == "exit"
+    break if convert_filename == "end"
     filenames << convert_filename
 end
+print 'outputfilename?(./refine_search/*)'
+newfilenames << STDIN.gets.chomp
 
-Parser.new(filenames)
+Parser.new(filenames).write(newfilename)
