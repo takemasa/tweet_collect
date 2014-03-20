009e60c405e1a6c0bcf7c25bc0761d1a9c702285
takemasa <takemasa@val.co.jp>
2014-03-20 14:17:24 +0900

    変換対象をファイルを.ltsvからltsv.gzに変更

--- bin/ltsv_parse_csv.rb
+++ bin/ltsv_parse_csv.rb
@@ -1,9 +1,10 @@
+# extract_transform内の.gzファイルを変換
 require_relative '../lib/parser.rb'
 require 'bundler/setup'
 Bundler.require(:report)
 
 filenames = []
-Dir.glob("./extract_transform/**/*.ltsv").each do |file|
+Dir.glob("./extract_transform/**/*.ltsv.gz").each do |file|
     filenames << file
 end
 
