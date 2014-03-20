c60c65dfb5f29d33ec3ef510f7e386a49a70d143
takemasa <takemasa@val.co.jp>
2014-03-20 21:06:06 +0900

    実行時に引数を要求するよう修正

--- bin/tweet_delete.rb
+++ bin/tweet_delete.rb
@@ -4,7 +4,9 @@ require_relative '../lib/uploader.rb'
 require 'bundler/setup'
 Bundler.require
 
-compressor = Compressor.new
+dir, extension = ARGV
+raise 'Target directory, Extension are required' unless extension
+compressor = Compressor.new(dir, extension)
 
 compressor.delete_old_ltsv
 compressor.delete_old_gzip
