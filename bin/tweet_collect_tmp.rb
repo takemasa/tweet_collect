0d14aa7015cdf7eb7e834bb3c792ab75b187c933
takemasa <takemasa@val.co.jp>
2014-04-17 16:22:31 +0900

    データ書き込み先ファイルの切り替えタイミングに合わせて実行プログラムを変更するため、修正前のコードを別ファイルに複製

---
+++ bin/tweet_collect_tmp.rb
@@ -0,0 +1,59 @@
+require_relative '../lib/collector.rb'
+require_relative '../lib/authenticater.rb'
+require_relative '../lib/writer.rb'
+require_relative '../lib/cleaner_tmp.rb'
+Dotenv.load
+group = ENV['group'] ? ENV['group'] : :default
+require 'bundler/setup'
+Bundler.require(group)
+
+(keyword, account_id, ary_type, result_type, lang) = ARGV
+writer = Writer.new(keyword)
+writer.make_dir
+client = Authenticater.new(account_id).get_twitter_client
+collector = Collector.new(keyword, result_type, lang)
+cleaner = Cleaner.new(ary_type)
+error_message = nil
+ary_all_tweets = []
+ary_id = []
+ary_log = []
+length = nil
+
+# 同時刻の多重起動を防ぐため取得の実行前にsleep
+sleep_time = account_id.to_i % 9
+sleep(sleep_time)
+execute_time = Time.now
+5.times do
+    tweet_id = nil
+    result = collector.search_tweet(client, execute_time)
+    # 出力先をツイートとエラーのどちらにするかを判断
+    result.class == Array ? tweets = result : error_message = result
+    if tweets
+        length = tweets.length
+        ary_log << length
+        tweets.each do |tweet|
+            ary_tweet = []
+            tweet_id = tweet[:id]
+         # ツイートが取得できていればそのidをインスタンス変数since_idに代入
+            collector.since_id = tweet_id if tweet_id
+
+            ary_tweet = cleaner.create_ary_tweet(tweet)
+            tweet = cleaner.join_tweet_status(ary_tweet)
+            ary_all_tweets << tweet
+        end
+    # ツイートが1件でも取得できていればそのidをid配列に格納
+        ary_id << tweet_id if tweet_id
+    # ファイルへの出力処理
+        writer.output_tweet(ary_all_tweets)
+        unless ary_id.empty?
+            collector.clear_idfile
+            writer.output_id(ary_id)
+        end
+    end
+    writer.output_error(error_message) unless error_message.nil?
+    sleep(10)
+    ary_all_tweets = []
+    ary_id = []
+    error_message = nil
+end
+writer.output_log(ary_log)
---
+++ lib/cleaner_tmp.rb
@@ -0,0 +1,126 @@
+class Cleaner
+
+    def initialize(ary_type = nil)
+        raise 'ary_type needed' unless ary_type
+        @ary_type = ary_type
+    end
+
+    def modify_tweet_status_str(tweet_text)
+        tweet_text.gsub(/(\r\n|\r|\n|\t|:|,)/," ")
+    end
+
+    def modify_twitter_client_str(twitter_client)
+        twitter_client.gsub(/(<.*">|<\/a>|\r\n|\r|\n|\t)/,"").gsub(","," ")
+    end
+
+    def modify_place(place = nil)
+        place ? [true, place.full_name] : [false, ""]
+    end
+
+    def modify_urls(tweet)
+        urls = []
+        tweet[:urls].each do |url|
+            urls << url[:expanded_url]
+        end
+        urls.join(' , ')
+    end
+
+    def retweeted_status(retweeted)
+        retweeted ? [true, retweeted.user.screen_name, retweeted.user.id, retweeted.id] : [false, nil, nil, nil]
+    end
+
+    def favorite_status(tweet, retweeted)
+        retweeted ? retweeted.favorite_count : tweet.favorite_count
+    end
+
+    def set_label(labels, tweet_status)
+        lists = {}
+        labels.each_with_index do |label, i|
+            lists[label] = "#{label}:#{tweet_status[i]}"
+        end
+        lists
+    end
+
+    def create_ary_tweet (tweet)
+        text_urls = tweet.attrs[:entities][:urls].empty? ? nil : modify_urls(tweet.attrs[:entities])
+        prof_urls = tweet.attrs[:user][:entities][:description][:urls].empty? ? nil : modify_urls(tweet.attrs[:user][:entities][:description])
+        home_urls = tweet.attrs[:user][:entities][:url] ? modify_urls(tweet.attrs[:user][:entities][:url]) : nil# ホームページのリンクが存在しない時は[:url]のハッシュが作られないため、空の配列を代入する
+        url = prof_urls && home_urls ? false : true
+        retweet = retweeted_status(tweet.retweeted_status)
+        tweet_place = modify_place(tweet.place)
+        favorite = favorite_status(tweet, tweet.retweeted_status)
+
+        labels = [
+            'created_at',
+            'tweet_id',
+            'tweet_page',
+            'text',
+            'text_url',
+            'user_name',
+            'user_id',
+            'user_page',
+            'profile',
+            'prof_url',
+            'home_url',
+            'client',
+            'place_status',
+            'place',
+            'friends_count',
+            'followers_count',
+            'all_tweet_count',
+            'listed_count',
+            'url_status',
+            'retweeted_status',
+            'retweet_count',
+            'retweeted_user',
+            'retweeted_user_id',
+            'retweeted_id',
+            'favorite_count'
+        ]
+
+
+        tweet_status = [
+            tweet.created_at,
+            tweet.id, #ツイートID
+            "https://twitter.com/#{tweet.user.screen_name}/status/#{tweet.id}",
+            modify_tweet_status_str(tweet.full_text), #ツイート本文
+            text_urls, #ツイートのURL
+            tweet.user.screen_name, #ツイートしたユーザ名
+            tweet.user.id, #ユーザID
+            "https://twitter.com/intent/user?user_id=#{tweet.user.id}", #ユーザーページのURL
+            modify_tweet_status_str(tweet.user.description), #ユーザのプロフィール
+            prof_urls, #プロフィール文のURL
+            home_urls, #プロフィールのURL欄
+            modify_twitter_client_str(tweet.source), #ツイート時に使用したクライアント
+            tweet_place[0], #位置情報の有無
+            tweet_place[1], #位置情報
+            tweet.user.friends_count, #フォロー数
+            tweet.user.followers_count, #フォロワー数
+            tweet.user.statuses_count, #総ツイート数
+            tweet.user.listed_count, #リストに加えられている数
+            url, # ツイート情報にURLが含まれているか
+            retweet[0], #リツイートであるか
+            tweet.retweet_count, #リツイートされた回数
+            retweet[1],#リツイート元のユーザ名
+            retweet[2],#リツイート元のユーザID
+            retweet[3],#リツイート元のツイートID
+            favorite
+        ]
+
+        tweet = set_label(labels, tweet_status)
+
+        case @ary_type
+        when 'simple'
+            ary = [tweet['created_at'], tweet['user_name'], tweet['text']]
+        when 'numeric'
+            ary = [tweet['created_at'], tweet['user_id'], tweet['tweet_id'], tweet['retweet_count'], tweet['friends_count'], tweet['followers_count'], tweet['all_tweet_count']]
+        when 'all'
+            ary = [tweet['created_at'], tweet['tweet_id'], tweet['tweet_page'], tweet['text'], tweet['text_url'], tweet['user_name'], tweet['user_id'], tweet['user_page'], tweet['profile'], tweet['prof_url'], tweet['home_url'], tweet['client'], tweet['place_status'], tweet['place'], tweet['friends_count'], tweet['followers_count'], tweet['all_tweet_count'], tweet['listed_count'], tweet['url_status'], tweet['retweeted_status'], tweet['retweet_count'], tweet['retweeted_user'], tweet['retweeted_user_id'], tweet['retweeted_id'], tweet['favorite_count']]
+        end
+        ary
+    end
+
+    def join_tweet_status(tweet)
+        tweet.join("\t")
+    end
+end
